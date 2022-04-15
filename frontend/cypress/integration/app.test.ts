a731e7ddc407d88bf9041cd6c864807e582ca938
AntoineAcquart <antoine.acquart@gmail.com>
2022-04-15 17:22:21 +0200

    ci

--- frontend/cypress/integration/app.test.ts
+++ frontend/cypress/integration/app.test.ts
@@ -1,17 +1,17 @@
 /// <reference types="cypress" />
 
 describe('App', () => {
-    // before(() => cy.task('mockServer', {
-    //     baseUrl: `http://localhost:8000/api`, mocks: [
-    //         { method: 'GET', route: '/api/cart', fixture: 'getCart.json' },
-    //         { method: 'POST', route: '/api/cart/1', fixture: 'addProduct.json' },
-    //         { method: 'POST', route: '/api/cart/2', fixture: 'addProductError.json' },
-    //         { method: 'DELETE', route: '/api/cart/1', fixture: 'removeProduct.json' },
-    //         { method: 'GET', route: '/api/products', fixture: 'getProducts.json' },
-    //     ]
-    // }))
+    before(() => cy.task('mockServer', {
+        baseUrl: `http://localhost:8000/api`, mocks: [
+            { method: 'GET', route: '/api/cart', fixture: 'getCart.json' },
+            { method: 'POST', route: '/api/cart/1', fixture: 'addProduct.json' },
+            { method: 'POST', route: '/api/cart/2', fixture: 'addProductError.json' },
+            { method: 'DELETE', route: '/api/cart/1', fixture: 'removeProduct.json' },
+            { method: 'GET', route: '/api/products', fixture: 'getProducts.json' },
+        ]
+    }))
     beforeEach(() => cy.visit('http://localhost:3000'))
-    // after(() => cy.task('stopServer'))
+    after(() => cy.task('stopServer'))
 
     it('common elements', () => {
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
